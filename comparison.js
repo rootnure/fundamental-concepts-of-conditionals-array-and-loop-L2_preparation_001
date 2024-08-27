5ebb08dc96b2766aff0f7f263b908e8b9d40c603
rootnure <nur.diu.2791@gmail.com>
2024-08-27 23:22:38 +0600

    comparison example in JS. Short note added in README.md

--- comparison.js
+++ comparison.js
@@ -11,3 +11,43 @@
  * 7. AND: &&
  * 8. OR: ||
  */
+
+console.log(5 > 10); // false
+console.log(5 < 10); // true
+console.log(5 == 10); // false
+console.log(10 == 10); // true
+console.log(5 >= 10); // false
+console.log(10 >= 10); // true
+console.log(5 <= 10); // true
+console.log(10 <= 10); // true
+
+/**
+ * COMPARISON USING VARIABLE
+ */
+var level1Fee = 5500;
+var level2Fee = 5500;
+
+console.log(level1Fee > level2Fee);
+console.log(level1Fee < level2Fee);
+console.log(level1Fee == level2Fee);
+console.log(level1Fee != level2Fee);
+console.log(level1Fee >= level2Fee);
+console.log(level1Fee <= level2Fee);
+
+/**
+ * SPECIAL COMPARISON
+ * == vs ===
+ * != vs !==
+ */
+
+console.log(10 == '10'); // true
+console.log(10 === '10'); // false
+
+console.log(5 == 10); // false
+console.log(5 === 10); // false
+
+console.log(10 != '10'); // false
+console.log(10 !== '10'); // true
+
+console.log(5 != '10'); // true
+console.log(5 !== '10'); // true
