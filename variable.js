a3c344a61d718341130a66dc9560b4d5e99d18aa
rootnure <nur.diu.2791@gmail.com>
2024-08-27 23:02:38 +0600

    variable in js

---
+++ variable.js
@@ -0,0 +1,42 @@
+/**
+ * VARIABLE ==> container
+ */
+
+/**
+ * 5 part of variable
+ * i. var/let/const keyword
+ * ii. name
+ * iii. equal sign (=)
+ * iv. value
+ * v. semicolon (;)
+ */
+
+// number type
+var age = 25;
+
+// string
+var name = "rootnure";
+
+// boolean
+var isWithTheFlow = false;
+
+/**
+ * NAMING CONVENTION
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword or reserve word
+ * 6. prefer camelCase
+ */
+
+var courseName = "Level 2 of pHero";
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN
+ * 4. null
+ * 5. Number
+ */
