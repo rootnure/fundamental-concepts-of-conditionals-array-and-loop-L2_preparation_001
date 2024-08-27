dd8baa637a0b05c9cd42f22d7c35cff88d22fab2
rootnure <nur.diu.2791@gmail.com>
2024-08-27 23:36:11 +0600

    conditionals in JS

---
+++ conditionals.js
@@ -0,0 +1,46 @@
+/**
+ * Course e registration korle
+ *  course content deya hobe
+ * 
+ * Course complete korle
+ *  certificate deya hobe
+ * 
+ * Pocket e taka thakle
+ *  cineplex e movie dekhte jaitam
+ */
+
+/**
+ * -----------STRUCTURE------------
+ * if (condition){
+ * 
+ * }
+ * 
+ */
+
+if(3 < 10) {
+    console.log('3 is less than 10');
+}
+
+var money = 400;
+
+if(money < 500) {
+    console.log('ajke movie dekhte jabo');
+}
+
+/**
+ * if(condition) {
+ *      execute code if condition is true
+ * }
+ * else {
+ *      execute code if condition is false
+ * }
+ */
+
+var weightInPound = 50;
+
+if(weightInPound < 40) {
+    console.log("I will carry by myself");
+}
+else {
+    console.log("I will rent a rickshaw");
+}
