db68fa20a55b5d98674f1d93501e057742d736a0
rootnure <nur.diu.2791@gmail.com>
2024-08-29 21:09:07 +0600

    For loop in JS

---
+++ for-loop.js
@@ -0,0 +1,15 @@
+/**
+ * for(loopVariable; loopCondition; loopVariableChange) {
+ *      // loop statement
+ * }
+ */
+
+// incremental for loop
+for(let num = 0; num <= 5; num++) {
+    console.log(num);
+}
+
+// decremental for loop
+for(let num = 5; num >= 0; num--) {
+    console.log(num);
+}
